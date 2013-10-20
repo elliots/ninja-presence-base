58db55b3e5a5dec618a114290beb4f4d4bfcf94d
Stephan van Rooij <stephan@svrooij.nl>
2013-10-20 12:54:59 +0200

    PresenceBase => Added state device
    I added a state device that should work with all driver that use this
    as a base driver.
    Only tested with ping-presence-base

--- index.js
+++ index.js
@@ -19,6 +19,48 @@ function Driver(opts,app) {
 
   this._timeouts = {};
   opts.lastValue = opts.LastValue || {};
+  
+  //All the devices discovered will be saved here, so we can emit the correct state.
+  this._allDevices = {};
+  
+  //State device
+  function PresenceStateDevice() {
+  	this.readable = true;
+  	this.writeable = true;
+  	this.V = 0;
+  	this.D = 244; //Generic state device
+  	this.G = "presence";
+  	var device = this;
+  	
+  	this._states = ['NobodyHome','SomeoneHome','EveryoneHome'];
+  	
+  }  
+  util.inherits(PresenceStateDevice,stream);
+  
+  PresenceStateDevice.prototype.actuateState = function(newState) {
+  	if(!this._state || this._state != newState){
+  		self._app.log.info('Presence => State changed from '+ this._state + ' to '+newState);
+  		this._state = newState;
+  		this.emit('data',newState);
+  	}
+  };
+  
+  PresenceStateDevice.prototype.write = function(data) {
+  	if(this._states.contains(data))
+  	{
+  		this.actuateState(data);
+  		return true;
+  	} else {
+  		return false;
+  	}
+  }
+  
+  //Used an array, in case more sub devices need to be added.
+  this.subDevices = {
+  	presenceState : new PresenceStateDevice()
+  };
+  
+  
 
   app.on('client::up',function(){
     if (!self.G) {
@@ -30,7 +72,14 @@ function Driver(opts,app) {
     if (self.D == undefined) {
       throw new Error('You must set "D" when creating a presence driver.');
     }
+    //Register current device
     self.emit('register', self);
+    //Register subdevice
+    Object.keys(self.subDevices).forEach(function(id) {
+		self._app.log.info('Adding sub-device', id, self.subDevices[id].G);
+		self.emit('register', self.subDevices[id]);
+	});
+    
     if (self.save) {
       self.save(); // May not be there in the test harness
     }
@@ -40,6 +89,8 @@ function Driver(opts,app) {
     if (self.scan) {
       self.startScanning();
     }
+    
+    
   });
 
 }
@@ -61,6 +112,10 @@ Driver.prototype.see = function(entity) {
   if (self._timeouts[entity.id]) {
     clearTimeout(self._timeouts[entity.id]);
   }
+  
+  if(!self._allDevices[entity.id]) {
+  	self._allDevices[entity.id] = true;
+  }
 
   if (!_.isEqual(this._opts.lastValue[entity.id], entity)) {
     self.emit('data', entity);
@@ -71,7 +126,32 @@ Driver.prototype.see = function(entity) {
       entity.present = entity['new'] = false;
       self.emit('data', entity);
       delete(self._timeouts[entity.id]);
+      self.sendPresenceState(); //after removing the entity from the timeout the state should change.
   }, this._opts.timeout);
+  
+  //We have seen a device, so the state could be updated.
+  self.sendPresenceState();
 };
+
+Driver.prototype.sendPresenceState = function(){
+	var self = this;
+	
+	//Get the number of objects in the timeout list.
+  	//Device get added and removed by the ninja-presence-base driver
+  	var currentOnlineHosts = Object.keys(self._timeouts).length;
+  	self._app.log.info('Presence => Number devices online: ' + currentOnlineHosts);
+  	self._app.log.info('Presence => All devices',self._allDevices);
+  	
+  	if(currentOnlineHosts == 0) // 0 devices in the timeout list = Nobody home
+	{
+		self.subDevices.presenceState.actuateState(self.subDevices.presenceState._states[0]);
+	} else if( currentOnlineHosts == Object.keys(self._allDevices).length) // All devices ever seen are online
+	{
+		self.subDevices.presenceState.actuateState(self.subDevices.presenceState._states[2]);
+	} else  //There are less devices in the timeout array then devices ever seen
+	{
+		self.subDevices.presenceState.actuateState(self.subDevices.presenceState._states[1]);
+	}
+}
 
 module.exports = Driver;
